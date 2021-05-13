6127e08669f087458a7de1d4d9308a54ebcf4edd
hellenvelis <hellenvelis@hotmail.com>
2021-05-12 22:59:17 -0300

    Ajuste de layout dos cards e inclusão de navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const apiURL = 'https://jsonplaceholder.typicode.com/posts'
-const infos = [{userId: 'userid', id: 'id', title: 'titulo', body: 'body'}]
+const infos = []
 const box = document.querySelector('#infos')
 
 fetch(apiURL)
@@ -16,10 +16,6 @@ fetch(apiURL)
 	template(infos)
 })
 
-infos.forEach(info => 
-	informacoes.innerHTML += (`<li>userid: ${info.userId} id: ${info.id} title: ${info.title} body ${info.body}</li>`)
-
-)
 
 
 function template(data)
@@ -30,11 +26,11 @@ function template(data)
 		content += `<div id="box">
 				<div class="card text-center">
   				<div class="card-header">
-   					 Featured
+   					 <b>${item.title}</b>
   				</div>
  				<div class="card-body">	
-   				 <h5 class="card-title">${item.title}</h5>
-   				 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+   				 <h5 class="card-title"></h5>
+   				 <p class="card-text"></p>
    				 <p>
 				  <button class="btn btn-outline-warning" type="button" data-toggle="collapse" data-target="#collapseExample${item.id}" aria-expanded="true" >
 				    See more
@@ -45,9 +41,6 @@ function template(data)
 				    ${item.body}
 				  </div>
 				</div>
-  				</div>
-  				<div class="card-footer text-muted">
-    			2 days ago
   				</div>
 				</div>
 			</div>
